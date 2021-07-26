1df4d87ad471991ee9975f9cce2acb2d90ba8e74
Amir Hyder <amirhyderkaloi@gmail.com>
2021-07-26 16:27:27 +0500

    binary tree using linked list

---
+++ src/com/systems/ltd/BinaryTreeNode.java
@@ -0,0 +1,13 @@
+package com.systems.ltd;
+
+//A binary tree node
+class BinaryTreeNode
+{
+	 int data;
+	 BinaryTreeNode left, right = null;
+	 BinaryTreeNode(int data)
+	 {
+	     this.data = data;
+	     left = right = null;
+	 }
+}
---
+++ src/com/systems/ltd/ListNode.java
@@ -0,0 +1,18 @@
+package com.systems.ltd;
+
+//Java program to create complete Binary Tree from its Linked List
+//representation
+
+
+
+//A linked list node
+class ListNode
+{
+	 int data;
+	 ListNode next;
+	 ListNode(int d)
+	 {
+	     data = d;
+	     next = null;
+	 }
+}
